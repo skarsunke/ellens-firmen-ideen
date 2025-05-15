0e10252c20e31d3f7130b624203567cf8eca63f8
skarsunke <skarsunke@googlemail.com>
2025-05-15 15:23:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
