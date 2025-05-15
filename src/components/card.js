508320d5936023cf6612e33005a5202719c9225f
skarsunke <skarsunke@googlemail.com>
2025-05-15 15:14:49 +0200

    Delete src/components/card.js

--- src/components/card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Card = ({ children, className }) => {
-  return (
-    <div className={`p-4 bg-white shadow-md rounded-lg ${className}`}>
-      {children}
-    </div>
-  );
-};
